1011a237853c943b38a8f23d9239e02f6fb7cbc0
ms31415926 <mike.shamilov@gmail.com>
2018-03-12 16:47:14 -0400

    pushed back up with changes

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { DashboardComponent } from './dashboard.component';
+import { HomeDashComponent } from './home-dash/home-dash.component';
+import { AccountDashComponent } from './account-dash/account-dash.component';
+import { FilesDashComponent } from './files-dash/files-dash.component';
+import { DeleteDashComponent } from './delete-dash/delete-dash.component';
+import { SharingDashComponent } from './sharing-dash/sharing-dash.component';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  exports: [
+    HomeDashComponent,
+    AccountDashComponent,
+    FilesDashComponent,
+    DeleteDashComponent,
+    SharingDashComponent,
+    DashboardComponent
+   ],
+  declarations: [
+    HomeDashComponent,
+    AccountDashComponent,
+    FilesDashComponent,
+    DeleteDashComponent,
+    SharingDashComponent,
+    DashboardComponent
+
+  ]
 })
 export class DashboardModule { }
