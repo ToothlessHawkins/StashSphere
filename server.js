ee8f976866d81470bb5e9675e9900dd1ad29812f
Patrick <kirchhoffpa@gmail.com>
2018-03-14 14:36:49 -0400

    added in tokens to the rest of the code

--- server.js
+++ server.js
@@ -231,6 +231,7 @@ app.delete("/user/delete", function (req, res) {
 updating a user's info. Pass the entire Json user in the body of the post request, provide password so the user verifies any change to their personal data. Exclude date_created, file paths, and associates list.
 formatting:
 {
+    token:<the provided login-token>,
     _username:<uname>,
     password:<pass>,
     fName:<first name>,
@@ -245,7 +246,7 @@ app.put("/user/update", function (req, res) {
         lName: req.body.lName,
     };
     User.findOneAndUpdate(
-        { _username: mod_user._username },
+        { token: jwt.decode(req.body.token, secret) },
         mod_user,
         function (err, user) {
             if (err) {
@@ -259,6 +260,7 @@ app.put("/user/update", function (req, res) {
 adding an associate is an update action, in the body of the request, provide the _username of the person to receive a new friend, and the _username of the person to become a new friend. The update will be reflexive.
 formatting:
 {
+    token:<provided login-token>,
     add_to:<_username of friend recipient>,
     add:<_username of friend to add>
 }
@@ -266,27 +268,34 @@ formatting:
 returns the "add_to" users's list of associates.
 */
 app.put("/user/add_friend", function (req, res) {
-    User.findOneAndUpdate(
-        { _username: req.body.add_to },
-        { $addToSet: { associates: req.body.add } },
+    User.findOne({ token: jwt.decode(req.body.token, secret) },
         function (err) {
             if (err) {
                 res.send(err);
-            }
-            User.findOneAndUpdate(
-                { _username: req.body.add },
-                { $addToSet: { associates: req.body.add_to } },
-                function (err) {
-                    if (err) {
-                        res.send(err);
+            } else {
+                User.findOneAndUpdate(
+                    { _username: req.body.add_to },
+                    { $addToSet: { associates: req.body.add } },
+                    function (err) {
+                        if (err) {
+                            res.send(err);
+                        }
+                        User.findOneAndUpdate(
+                            { _username: req.body.add },
+                            { $addToSet: { associates: req.body.add_to } },
+                            function (err) {
+                                if (err) {
+                                    res.send(err);
+                                }
+                            }
+                        );
                     }
-                }
-            );
-        }
-    );
-    User.findOne({ _username: req.body.add_to }, function (err, resu) {
-        res.json(resu.associates);
-    });
+                );
+                User.findOne({ _username: req.body.add_to }, function (err, resu) {
+                    res.json(resu.associates);
+                });
+            }
+        });
 });
 
 /*node actions*/
@@ -295,6 +304,7 @@ app.put("/user/add_friend", function (req, res) {
 uploading a file, some information must be provided along with the file itself. Note that the current path should end with a "/".
 formatting:
 {
+    token:<the provided login-token>,
     u_path:<path (from user's root) that ends with the target folder>,
     owner:<the _username of the uploader>,
     file:<the file to store>
@@ -308,83 +318,85 @@ app.post("/node/file_up", upload.single("file"), function (req, res) {
     console.log("path: " + req.body.u_path);
     console.log("owner: " + req.body.owner);
     console.log("original name: " + req.file.originalname);
-    User.findOne({ _username: req.body.owner }, function (err, user) {
-        if (err) { res.send(err); }
-        if (user) {
-            var from = "./temp/" + req.file.originalname;
-            var to = user.path_root + "/" + req.body.u_path + req.file.originalname;
-            console.log("to: " + to);
-            fs.copyFileSync(from, to);
-            if (fs.existsSync(to)) {
-                //the file exists in the new directory, clean up temp
-                fs.unlinkSync(from);
-                //add a reference to the new file in the database
-                Node.create({
-                    file_name: to,
-                    uploader: req.body.owner,
-                    date_up: Date.now(),
-                    children: [],
-                    starred: false,
-                    deleted: false,
-                    isFolder: false
-                });
-                res.json({ "created": true, "location": to });
-            } else {
-                res.json({
-                    "created": false,
-                    "message": "File does not exist in desired directory."
-                });
+    User.findOne({ token: jwt.decode(req.body.token, secret) },
+        function (err, user) {
+            if (err) { res.send(err); }
+            if (user) {
+                var from = "./temp/" + req.file.originalname;
+                var to = user.path_root + "/" + req.body.u_path + req.file.originalname;
+                console.log("to: " + to);
+                fs.copyFileSync(from, to);
+                if (fs.existsSync(to)) {
+                    //the file exists in the new directory, clean up temp
+                    fs.unlinkSync(from);
+                    //add a reference to the new file in the database
+                    Node.create({
+                        file_name: to,
+                        uploader: req.body.owner,
+                        date_up: Date.now(),
+                        children: [],
+                        starred: false,
+                        deleted: false,
+                        isFolder: false
+                    });
+                    res.json({ "created": true, "location": to });
+                } else {
+                    res.json({
+                        "created": false,
+                        "message": "File does not exist in desired directory."
+                    });
+                }
             }
-        }
-    });
+        });
 });
 
 /*
 creating a folder. Provide the _username, "current path" and the name of the folder to be created.
 formatting:
 {
-    "_username":<_username>,
+    "token":<the provided login-token>,
     "cur_path":<user's current path (from their root folder)>,
     "name":<the name of the folder to be created>
 }
 */
 app.post("/node/new_folder", function (req, res) {
     console.log("received request to create a folder:\n" + req.body.name + "\nin the existing folder:\n" + req.body.cur_path + "\nfor user: " + req.body._username);
-    User.findOne({ _username: req.body._username }, function (err, user) {
-        if (err) {
-            res.send(err);
-        } else {
-            var dest = user.path_root + "/" + req.body.cur_path + req.body.name;
-            fs.mkdirSync(dest);
-            if (fs.existsSync(dest)) {
-                var f_doc = new Node();
-                f_doc.file_name = dest;
-                f_doc.uploader = req.body._username;
-                f_doc.date_up = Date.now();
-                f_doc.children = [];
-                f_doc.starred = false;
-                f_doc.deleted = false;
-                f_doc.isFolder = true;
-                f_doc.save(function (err, doc) {
-                    Node.findOneAndUpdate(
-                        { file_name: user.path_root + "/" + req.body.cur_path.slice(0, -1) },
-                        { $addToSet: { children: doc._id } },
-                        function (err) { if (err) { res.send(err); } }
-                    );
-                    res.json(
-                        {
-                            "created": true,
-                            "location": dest
-                        });
-                });
+    User.findOne({ token: jwt.decode(req.body.token, secret) },
+        function (err, user) {
+            if (err) {
+                res.send(err);
             } else {
-                res.json({
-                    "created": false,
-                    "message": "after trying to create the directory, it did not exist in the desired path"
-                }); //end bad response
-            } //end if-else-if: fs.existsSync()
-        } //end if-else-if err
-    }); //end findone
+                var dest = user.path_root + "/" + req.body.cur_path + req.body.name;
+                fs.mkdirSync(dest);
+                if (fs.existsSync(dest)) {
+                    var f_doc = new Node();
+                    f_doc.file_name = dest;
+                    f_doc.uploader = req.body._username;
+                    f_doc.date_up = Date.now();
+                    f_doc.children = [];
+                    f_doc.starred = false;
+                    f_doc.deleted = false;
+                    f_doc.isFolder = true;
+                    f_doc.save(function (err, doc) {
+                        Node.findOneAndUpdate(
+                            { file_name: user.path_root + "/" + req.body.cur_path.slice(0, -1) },
+                            { $addToSet: { children: doc._id } },
+                            function (err) { if (err) { res.send(err); } }
+                        );
+                        res.json(
+                            {
+                                "created": true,
+                                "location": dest
+                            });
+                    });
+                } else {
+                    res.json({
+                        "created": false,
+                        "message": "after trying to create the directory, it did not exist in the desired path"
+                    }); //end bad response
+                } //end if-else-if: fs.existsSync()
+            } //end if-else-if err
+        }); //end findone
 });
 
 /*
@@ -419,15 +431,14 @@ app.put("/node/star", function (req, res) {
 getting a file. Must provide username/password to verify that they can view the files, the name of the file they want (will return a file object), and the current path.
 Formatting:
 {
-    _username:<the user's _username>,
-    password:<the user's password>,
+    token:<the provided login-token>,
     file_name:<the name of the file the user clicked on>,
     cur_path:<the path from root to the directory the user is in currently>
 }
 */
 app.post("/node/file", function (req, res) {
     User.findOne(
-        { _username: req.body._username, password: req.body.password },
+        { token: jwt.decode(req.body.token, secret) },
         function (err, user) {
             if (err) {
                 res.send(err);
@@ -464,15 +475,14 @@ app.post("/node/file", function (req, res) {
 getting a folder. must provide the username/password for verification, the name of the folder they want to view, and the current path, from root.
 formatting:
 {
-    _username:<the user's _username>,
-    password:<the user's password>,
+    token:<the provided login-token>,
     folder_name:<the folder the user is clicking on>,
     cur_path:<the path from root to the directory the user is currently in>
 }
 */
 app.post("/node/folder", function (req, res) {
     User.findOne(
-        { _username: req.body._username, password: req.body.password },
+        { token: jwt.decode(req.body.token, secret) },
         function (err, user) {
             if (err) {
                 res.send(err);
