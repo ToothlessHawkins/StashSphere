a40812dbc50c6656573185980b3abe0822549931
ms31415926 <mike.shamilov@gmail.com>
2018-03-13 23:34:17 -0400

    minor design changes to home/account dash

--- src/app/dashboard/home-dash/home-dash.component.ts
+++ src/app/dashboard/home-dash/home-dash.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const jquery: any;
+declare const $: any;
+
 @Component({
   selector: 'app-home-dash',
   templateUrl: './home-dash.component.html',
   styleUrls: ['./home-dash.component.css']
 })
 export class HomeDashComponent implements OnInit {
-
+  btn = ['Hide','Hide'];
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleHome(x) {
+    let tbl = ['.show-fav-tbl', '.show-recent-tbl']
+
+    if (this.btn[x] === 'Hide') {
+      this.btn[x] = 'Show';
+    } else {
+      this.btn[x] = 'Hide'
+    }
+
+    $(tbl[x]).toggle();
+  }
 }
